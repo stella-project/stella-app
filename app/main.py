9ebc6031ea572e5efac487ea0054ad7b68265b8d
Benjamin Wolff <wolff@zbmed.de>
2019-10-30 13:43:42 +0100

    add changes

--- app/main.py
+++ app/main.py
@@ -4,38 +4,44 @@ import json
 import docker
 from flask import Flask, jsonify, request
 
+import logging
+import utils
+from config import conf
+
+utils.create_logger("stella-app",f"{conf['log']['log_path']}/{conf['log']['log_file']}")
+logger = logging.getLogger("stella-app")
+logger.info("Logging started!")
+
+client = docker.DockerClient(base_url='unix://var/run/docker.sock')
 app = Flask(__name__)
 
-container_dict = {"lucene": {"requests": 0},
-                  "solr": {"requests": 0}}
-
-
 def get_least_served(container_dict):
+    ''' return least served container '''
     least_served = list(container_dict.keys())[0]
     requests = container_dict[least_served]["requests"]
 
     for entry in container_dict.keys():
         if container_dict[entry]['requests'] < requests:
             least_served = entry
-
+    logger.debug(f'least served container is: {least_served}')
     container_dict[least_served]['requests'] += 1
     return least_served
 
-
 def index():
-    client = docker.DockerClient(base_url='unix://var/run/docker.sock')
-    container_list = list(container_dict.keys())
+    ''' run indexing methods for all available containers '''
+    container_list = list(conf["app"]["container_dict"].keys())
 
     cmd = 'python /script/index'
 
     for entry in container_list:
         container = client.containers.get(entry)
+        logger.debug(f'Indexing container "{container}"...')
         exec_res = container.exec_run(cmd)
 
 
 @app.route("/")
 def home():
-    client = docker.DockerClient(base_url='unix://var/run/docker.sock')
+    ''' display running containers '''
 
     html = "<h1> Here is a list of containers that are running: </h1> </br> <ul>"
 
@@ -62,15 +68,15 @@ def home():
 
 @app.route("/stella/api/site/ranking/<string:query>", methods=["GET"])
 def ranking(query):
-    client = docker.DockerClient(base_url='unix://var/run/docker.sock')
-    least_served = get_least_served(container_dict)
+    ''' return ranked documents in JSON-Format produced by least served container '''
+    least_served = get_least_served(conf["app"]["container_dict"])
 
     container = client.containers.get(least_served)
 
     cmd = 'python /script/search ' + query
 
+    logger.debug(f'produce ranking with container: "{least_served}"...')
     exec_res = container.exec_run(cmd)
-
     exec_dict = json.loads(exec_res.output.decode("utf-8"))
 
     return jsonify(exec_dict)
@@ -78,8 +84,8 @@ def ranking(query):
 
 @app.route("/stella/api/site/test/<string:container_name>", methods=["GET"])
 def test(container_name):
+    ''' run test script for given container name'''
     if request.method == 'GET':
-        client = docker.DockerClient(base_url='unix://var/run/docker.sock')
         container = client.containers.get(container_name)
 
         cmd = 'python /script/test'
@@ -101,4 +107,5 @@ def feedback(site_id):
 
 if __name__ == '__main__':
     index()
+    logger.info("Starting app...")
     app.run(host='0.0.0.0', port=80, debug=False)
