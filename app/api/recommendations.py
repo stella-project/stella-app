bee2c2f8aad72884e6aaa11682985c8f8a63eadd
breuert <timo.breuer@th-koeln.de>
2019-12-16 14:38:54 +0100

    add endpoint of livivo recommender

--- app/api/recommendations.py
+++ app/api/recommendations.py
@@ -1,2 +1,24 @@
+import docker
+import logging
+import json
+from flask import jsonify, request
+from . import api
+from core import get_least_served
+from config import conf
 
+client = docker.DockerClient(base_url='unix://var/run/docker.sock')
+
+
+@api.route("/recommendation/<string:docid>", methods=["GET"])
+def recommendation(docid):
+    container = client.containers.get('livivo-solr')
+
+    cmd = 'python /script/recommend ' + docid
+
+    logger = logging.getLogger("stella-app")
+    logger.debug(f'produce recommendation with container: livivo-solr"...')
+    exec_res = container.exec_run(cmd)
+    exec_dict = json.loads(exec_res.output.decode("utf-8"))
+
+    return jsonify(exec_dict)
 
