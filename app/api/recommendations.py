f6a50334ceac59e7ac7b5ac4b5b8f34b7fbb903f
breuert <timo.breuer@th-koeln.de>
2020-01-30 13:56:50 +0100

    add local recommender system

--- app/api/recommendations.py
+++ app/api/recommendations.py
@@ -5,20 +5,41 @@ from flask import jsonify, request
 from . import api
 from core import get_least_served
 from config import conf
+import ast
 
 client = docker.DockerClient(base_url='unix://var/run/docker.sock')
 
 
-@api.route("/recommendation/<string:docid>", methods=["GET"])
-def recommendation(docid):
-    container = client.containers.get('livivo-solr')
-
-    cmd = 'python3 /script/recommend ' + docid
-
+@api.route("/recommend_dataset/<string:doc_id>", methods=["GET"])
+def recommend_dataset(doc_id):
+    least_served = get_least_served(conf["app"]["container_dict_recommendation"])
+    client = docker.DockerClient(base_url='unix://var/run/docker.sock')
+    container = client.containers.get(least_served)
+    cmd = 'python3 /script/recommend_dataset ' + doc_id
     logger = logging.getLogger("stella-app")
-    logger.debug(f'produce recommendation with container: livivo-solr"...')
+    logger.debug(f'produce ranking with container: "{least_served}"...')
+
     exec_res = container.exec_run(cmd)
-    exec_dict = json.loads(exec_res.output.decode("utf-8"))
 
-    return jsonify(exec_dict)
+    json_data = {}
+    json_data = ast.literal_eval(exec_res.output.decode("utf-8"))
+    json_data.update({'container': least_served})
+    return jsonify(json_data)
 
+
+@api.route('/dashboard')
+def dashboard():
+    entities = {"Research_data": resources.get_datasets_len(), "Publication": resources.get_publications_len()}
+    itemetadata = resources.get_dataset_metadata().iloc[0]['_source']
+    return render_template('dashboard.html', entities=entities, itemetadata=itemetadata)
+
+
+@api.route('/', methods=['GET'])
+def get():
+    message = "GWS RecSys APP"
+    return render_template('base.html', message=message)
+
+
+@api.errorhandler(404)
+def page_not_found(error):
+    return render_template('page_not_found.html'), 404
--- app/config.py
+++ app/config.py
@@ -3,11 +3,16 @@ conf = {}
 # app settings
 conf["app"] = {}
 conf["app"]["container_list"] = ["elastic", "elastic-external"]
+conf["app"]["container_list_recommendation"] = ["recomm-sample"]
 
 # create container_dict from list values (easier)
 conf["app"]["container_dict"] = {}
 for container_name in conf["app"]["container_list"]:
     conf["app"]["container_dict"][container_name] = {"requests":0}
+
+conf["app"]["container_dict_recommendation"] = {}
+for container_name in conf["app"]["container_list_recommendation"]:
+    conf["app"]["container_dict_recommendation"][container_name] = {"requests":0}
 
 # logger settings
 conf["log"] = {}
