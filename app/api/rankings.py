f2fb52f763a7c47ec2674610889c16ac80b610c7
Benjamin Wolff <wolff@zbmed.de>
2020-02-11 15:10:30 +0100

    Updated ranking data structure

--- app/api/rankings.py
+++ app/api/rankings.py
@@ -24,34 +24,44 @@ def test(container_name):
         return "<h1> " + out.output.decode("utf-8") + " </h1>"
 
 
-@api.route("/ranking/<string:query>", methods=["GET"])
-def ranking(query):
+@api.route("/ranking", methods=["GET"])
+def ranking():
     logger = logging.getLogger("stella-app")
-    ''' return ranked documents in JSON-Format produced by least served container '''
-    least_served = get_least_served(conf["app"]["container_dict"])
-
-    container = client.containers.get(least_served)
-
-    page = request.args.get('page', default=0,type=int)
-    results_per_page = request.args.get('results_per_page', default=20,type=int)
+    query = request.args.get('query',None)
+    container_name = request.args.get('container',None)
     
-    cmd = 'python3 /script/search {} {} {}'.format(query,results_per_page,page) 
-    logger.debug('cmd: {}'.format(cmd))
-     
-    logger.debug(f'produce ranking with container: "{least_served}"...')
-    ts_start =  time.time()
-    ts = round(ts_start*1000)
-    exec_res = container.exec_run(cmd)
-    logger.debug(f'exec_res: {exec_res.output.decode("utf-8")}')
-    result = json.loads(exec_res.output.decode('utf-8'))
-    ts_end = time.time()
-    # calc query execution time in ms
-    q_time = round((ts_end-ts_start)*1000)
-    
-    num_found = len(result['itemlist'])
+    if(container_name is None):
+        container_name = get_least_served(conf["app"]["container_dict"])
+    else:
+        # check if container name exists
+        if not (container_name in conf["app"]["container_dict"]):
+            return "TODO: Implement"
+            
+    container = client.containers.get(container_name)
 
-    # TODO implement paging based on get-params
+    logger.debug(f'produce ranking with container: "{container_name}"...')
 
-    response_dict = create_dict_response(itemlist=result['itemlist'],params=request.args,q_time=q_time,container=least_served,num_found=num_found,ts=ts,page=page,results_per_page=results_per_page,query=query)
+    if(query is None):
+        return "TODO: Implement"
+    else:
+        ''' return ranked documents in JSON-Format produced by least served container '''
+
+        #requests.args.get('container',default=False,type=boolean)
+
+        page = request.args.get('page', default=0,type=int)
+        rpp = request.args.get('rpp', default=20,type=int)
+        
+        cmd = 'python3 /script/search {} {} {}'.format(query,rpp,page) 
+        logger.debug('cmd: {}'.format(cmd))
+         
+        ts_start =  time.time()
+        ts = round(ts_start*1000)
+        exec_res = container.exec_run(cmd)
+        logger.debug(f'exec_res: {exec_res.output.decode("utf-8")}')
+        result = json.loads(exec_res.output.decode('utf-8'))
+        ts_end = time.time()
+        # calc query execution time in ms
+        q_time = round((ts_end-ts_start)*1000)
+        response_dict = create_dict_response(itemlist=result['itemlist'],params=request.args,q_time=q_time,container=container_name,num_found=result['num_found'],ts=ts,page=page,rpp=rpp,query=query)
     
     return jsonify(response_dict)
--- app/utils.py
+++ app/utils.py
@@ -4,7 +4,7 @@ def create_logger(name, file, loglevel=logging.NOTSET):
 	logging.basicConfig(level=loglevel,filename=file, format='%(asctime)s %(levelname)s %(filename)s %(funcName)s - %(message)s')
 
 def create_dict_response(status=0, q_time=0, container='', params={}, sid='',rid='',ts=0, 
-						 num_found=0, page=0, results_per_page=20, itemlist=[], query=''):
+						 num_found=0, page=0, rpp=20, itemlist=[], query=''):
 	dict_response = {}
 	dict_response['response_header'] = {}
 	dict_response['response'] = {}
@@ -19,20 +19,20 @@ def create_dict_response(status=0, q_time=0, container='', params={}, sid='',rid
 	if "page" in params:
 		page = int(params['page'])
 
-	if "results_per_page" in params:
-		results_per_page = int(params['results_per_page'])
+	if "rpp" in params:
+		rpp = int(params['rpp'])
 	'''
 	dict_response['response']['num_found'] = num_found
 	dict_response['response']['page'] = page
-	dict_response['response']['results_per_page'] = results_per_page
-	#i = page*results_per_page
-	#j = page*results_per_page+results_per_page
+	dict_response['response']['rpp'] = rpp
+	#i = page*rpp
+	#j = page*rpp+rpp
 
 	dict_response['response']['itemlist'] = itemlist
 
 	#try:
 	#	dict_response['response']['itemlist'] = itemlist[i:j]
 	#except:
-	#	dict_response['response']['itemlist'] = itemlist[0,results_per_page]
+	#	dict_response['response']['itemlist'] = itemlist[0,rpp]
 
 	return dict_response  
