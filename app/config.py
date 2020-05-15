bc1e96d8cd0ea764d3b05b44bf324c67e7da7476
breuert <timo.breuer@th-koeln.de>
2020-05-15 14:01:17 +0200

    add baseline recommender

--- app/config.py
+++ app/config.py
@@ -5,7 +5,7 @@ conf["app"] = {}
 conf["app"]["container_list"] = ["livivo_elastic", "livivo_elastic_base"]
 conf["app"]["container_list_recommendation"] = ["recom_tfidf"]
 conf["app"]["container_baseline"] = "livivo_elastic_base"
-conf["app"]["container_recommendation_baseline"] = "recom_tfidf"
+conf["app"]["container_recommendation_baseline"] = "recom_tfidf_base"
 
 # create container_dict from list values (easier)
 conf["app"]["container_dict"] = {}
---
+++ recom_tfidf_base/app.py
@@ -0,0 +1,91 @@
+from flask import Flask, request, jsonify
+import json
+import pickle
+import random
+
+JL_PATH = './data/pubmed_2015_2016.jsonl'
+app = Flask(__name__)
+cos_sim_mat = pickle.load(open("cosine_similarity_matrix_top_10.pkl", "rb"))
+
+
+def index_data(jl_path):
+    corpus = {}
+    with open(jl_path) as jl:
+        for line in jl:
+            j = json.loads(line)
+            corpus[j['DBRECORDID']] = json.loads(line)
+    return corpus
+
+
+@app.route('/test', methods=["GET"])
+def test():
+    ######################
+    ### CUSTOM - START ###
+    ######################
+    print("tfidf vectorizer!")
+    ####################
+    ### CUSTOM - END ###
+    ####################
+
+
+@app.route('/index', methods=["GET"])
+def index():
+    ######################
+    ### CUSTOM - START ###
+    ######################
+    pass
+    ####################
+    ### CUSTOM - END ###
+    ####################
+
+
+@app.route('/recommendation/datasets', methods=["GET"])
+def rec_data():
+    item_id = request.args.get('item_id', None)
+    page = request.args.get('page', default=0, type=int)
+    rpp = request.args.get('rpp', default=20, type=int)
+
+    
+    response = {}
+    response['page'] = page
+    response['rpp'] = rpp
+    response['item_id'] = item_id
+    response['num_found'] = 0
+    response['itemlist'] = []
+    
+    result = [sim for sim in cos_sim_mat if sim["target_items"]==item_id][0]['similar_items']
+    response['itemlist'] = [{"id":k, "detail":{"score": v,"reason":"" }} for k, v in result.items()]
+
+    #pass
+    ####################
+    ### CUSTOM - END ###
+    ####################
+
+    return jsonify(response)
+
+
+@app.route('/recommendation/publications', methods=["GET"])
+def rec_pub():
+    item_id = request.args.get('item_id', None)
+    page = request.args.get('page', default=0, type=int)
+    rpp = request.args.get('rpp', default=20, type=int)
+
+    response = {}
+    response['page'] = page
+    response['rpp'] = rpp
+    response['item_id'] = item_id
+    response['num_found'] = 0
+    ######################
+    ### CUSTOM - START ###
+    ######################
+    response['itemlist'] = random.choices(list(corpus.keys()), k=rpp)
+    ####################
+    ### CUSTOM - END ###
+    ####################
+
+    return jsonify(response)
+
+
+if __name__ == '__main__':
+    corpus = index_data(JL_PATH)
+    app.run(host='0.0.0.0', port=5000, debug=True)
