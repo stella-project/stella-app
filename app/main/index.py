a2da94f7238d6332c2dffe5685373adb9bd8e57c
breuert <timo.breuer@th-koeln.de>
2020-05-27 14:59:10 +0200

    add API endpoint for bulk indexing

--- app/main/index.py
+++ app/main/index.py
@@ -1,3 +1,4 @@
+import threading
 from . import main
 from config import conf
 from core.index import rest_index, cmd_index
@@ -9,3 +10,19 @@ def index(container_name):
         rest_index(container_name)
     else:
         cmd_index(container_name)
+
+
+@main.route("/index/bulk")
+def index_bulk():
+    container_list = conf["app"]["container_list"] + conf["app"]["container_list_recommendation"]
+    threads = []
+    if conf['app']['REST_QUERY']:
+        for container_name in container_list:
+            t = threading.Thread(target=rest_index, args=(container_name,))
+            threads.append(t)
+            t.start()
+    else:
+        for container_name in container_list:
+            t = threading.Thread(target=cmd_index, args=(container_name,))
+            threads.append(t)
+            t.start()
