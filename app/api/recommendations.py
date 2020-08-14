65a460933dce1c2ad8f2796ab7aa44fccbc4a44b
narges1212 <narges.1212@gmail.com>
2020-08-14 17:48:08 +0200

    Code documentation recommendation  #16

--- app/api/recommendations.py
+++ app/api/recommendations.py
@@ -16,6 +16,16 @@ client = docker.DockerClient(base_url='unix://var/run/docker.sock')
 
 
 def new_session(container_name, container_rec_name):
+    '''
+    create a new session which sets container_name as a ranking system and container_rec_name as a recommendation system
+    and return session_id
+    Args:
+        container_name: container name of ranking system
+        container_rec_name: container name of recommendation system
+
+    Returns:
+        session_id: the created session id
+    '''
     session = Session(start=datetime.now(),
                       system_ranking=System.query.filter_by(name=container_name).first().id,
                       system_recommendation=System.query.filter_by(name=container_rec_name).first().id,
@@ -29,6 +39,17 @@ def new_session(container_name, container_rec_name):
 
 
 def rest_rec_data(container_name, item_id, rpp, page):
+    '''
+    get the container_name, item_id , rpp and page number and return a ranking datasets produced by container_name
+    Args:
+        container_name: name of container to produce recommendation
+        item_id: item_id as seed for recommendation
+        rpp: rank par page
+        page: page number
+
+    Returns:
+        dataset recommendation ranking for item_id
+    '''
     if conf['app']['DEBUG']:
         container = client.containers.get(container_name)
         ip_address = container.attrs['NetworkSettings']['Networks']['stella-app_default']['IPAddress']
@@ -45,6 +66,17 @@ def cmd_rec_data(container_name, query, rpp, page):
 
 
 def rest_rec_pub(container_name, item_id, rpp, page):
+    '''
+    get the container_name, item_id , rpp and page number and return a ranking publications produced by container_name
+    Args:
+        container_name: name of container to produce recommendation
+        item_id: item_id as seed for recommendation
+        rpp: rank par page
+        page: page number
+
+    Returns:
+        publications recommendation ranking for item_id
+    '''
     if conf['app']['DEBUG']:
         container = client.containers.get(container_name)
         ip_address = container.attrs['NetworkSettings']['Networks']['stella-app_default']['IPAddress']
@@ -63,6 +95,21 @@ def cmd_rec_pub():
 
 
 def query_system(container_name, item_id, rpp, page, session_id, logger, type='EXP', rec_type='DATA'):
+    '''
+
+    Args:
+        container_name: container_name set to produce the recommendation
+        item_id: id of seed item for recommendation
+        rpp: ranking per page
+        page: page number
+        session_id: session id
+        logger: logger
+        type: type of container
+        rec_type: type pf recommended items
+
+    Returns:
+        for session_id returns recommendation result of rec_type
+    '''
     logger.debug(f'produce recommendation with container: "{container_name}"...')
     q_date = datetime.now().replace(microsecond=0)
     ts_start = time.time()
@@ -104,6 +151,16 @@ def query_system(container_name, item_id, rpp, page, session_id, logger, type='E
 
 
 def interleave(recommendation_exp, recommendation_base, rec_type='DATA'):
+    '''
+    interleave the experimental with base ranking for recommending item types rec_type
+    Args:
+        recommendation_exp: ranking of experimental system
+        recommendation_base: ranking of base system
+        rec_type: type of recommended items
+
+    Returns:
+        a list of items produced experimental system interleaved with base ranking
+    '''
     base = {k: v.get('docid') for k, v in recommendation_base.items.items()}
     exp = {k: v.get('docid') for k, v in recommendation_exp.items.items()}
 
@@ -140,6 +197,14 @@ def interleave(recommendation_exp, recommendation_base, rec_type='DATA'):
 
 
 def single_recommendation(recommendation):
+    '''
+
+    Args:
+        recommendation:
+
+    Returns:
+
+    '''
     db.session.add(recommendation)
     db.session.commit()
 
@@ -148,6 +213,14 @@ def single_recommendation(recommendation):
 
 @api.route('/recommendation/<int:id>/feedback', methods=['POST'])
 def post_rec_feedback(id):
+    '''
+    post feedback of ranking for recommendation with identifier id
+    Args:
+        id: identifier of ranking for recommendation
+
+    Returns:
+        post message
+    '''
     clicks = request.values.get('clicks', None)
     if clicks is not None:
         recommendation = Result.query.get_or_404(id)
@@ -172,6 +245,10 @@ def post_rec_feedback(id):
 
 @api.route("/recommendation/datasets", methods=["GET"])
 def recommend_dataset():
+    '''
+    Returns:
+        ranked list for recommending dataset for itemid
+    '''
     logger = logging.getLogger("stella-app")
     # look for mandatory GET-parameters (query, container_name)
     itemid = request.args.get('itemid', None)
@@ -237,6 +314,11 @@ def recommend_dataset():
 
 @api.route("/recommendation/publications", methods=["GET"])
 def recommend():
+    '''
+
+    Returns:
+        ranked list for recommending publications for itemid
+    '''
     logger = logging.getLogger("stella-app")
     # look for mandatory GET-parameters (query, container_name)
     itemid = request.args.get('itemid', None)
