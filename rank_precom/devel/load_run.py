0ba67e4b68c5e4f7327ed496e95555b363551fb6
breuert <timo.breuer@th-koeln.de>
2020-11-05 13:40:56 +0100

    rm folders with precompute templates

--- rank_precom/devel/load_run.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv('./precomputed.txt', sep=' ', names=['num', 'Q0', 'rank', 'docid', 'score', 'runid'])
-
-pass
--- rank_precom_test/devel/load_xml.py
+++
@@ -1,7 +0,0 @@
-import xml.etree.ElementTree as ET
-
-tree = ET.parse('head_queries.xml')
-
-head_queries = [hq.find('query').text for hq in tree.findall('head_query')]
-
-pass
