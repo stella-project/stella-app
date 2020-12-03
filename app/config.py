8992a4943ac385b92ddf41477c75acb0e66f50e6
breuert <timo.breuer@th-koeln.de>
2020-12-03 12:25:50 +0100

    set DEBUG to False; BULK_INDEX to True

--- app/config.py
+++ app/config.py
@@ -10,7 +10,7 @@ if os.environ.get("RANKSYS_LIST"):
 else:
     conf["app"]["container_list"] = ["livivo_base", "livivo_rank_precom", "livivo_rank_pyserini", "livivo_rank_pyterrier"]  # optionally hard-code a list with experimental ranking systems (cf. recsys)
 
-conf["app"]["container_baseline"] = os.environ.get("RANKSYS_BASE") or "livivo_base"# optionally provide a baseline system (cf. recsys)
+conf["app"]["container_baseline"] = os.environ.get("RANKSYS_BASE") or "livivo_base" # optionally provide a baseline system (cf. recsys)
 
 if os.environ.get("RECSYS_LIST"):
     conf["app"]["container_list_recommendation"] = [recsys for recsys in os.environ.get("RECSYS_LIST").split(" ")]
@@ -28,7 +28,7 @@ conf["app"]["container_dict_recommendation"] = {}
 for container_name in conf["app"]["container_list_recommendation"]:
     conf["app"]["container_dict_recommendation"][container_name] = {"requests": 0}
 
-conf['app']['DEBUG'] = True
+conf['app']['DEBUG'] = False
 conf['app']['DELETE_SENT_SESSION'] = True
 conf["app"]["INTERVAL_DB_CHECK"] = 3  # seconds
 conf["app"]["SESSION_EXPIRATION"] = 6  # seconds
@@ -48,7 +48,7 @@ conf["app"]["STELLA_SERVER_USERNAME"] = "GESIS"
 
 conf["app"]["INTERLEAVE"] = True
 conf['app']['REST_QUERY'] = True
-conf['app']['BULK_INDEX'] = False
+conf['app']['BULK_INDEX'] = True
 
 # logger settings
 conf["log"] = {}
