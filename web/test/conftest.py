ea33becea1110515a1c2151e154044f2cd82137e
jueri <jueri.keller@th-koeln.de>
2024-09-19 17:05:29 +0200

    update docker version in mock

--- web/test/conftest.py
+++ web/test/conftest.py
@@ -109,7 +109,7 @@ def mock_request_base_system(requests_mock):
 
     # Mock the debug docker network endpoint
     requests_mock.get(
-        f"http+docker://localhost/v1.46/containers/{container_name}/json",
+        f"http+docker://localhost/v1.47/containers/{container_name}/json",
         json={
             "NetworkSettings": {
                 "Networks": {"stella-app_default": {"IPAddress": container_name}}
@@ -133,7 +133,7 @@ def mock_request_experimental_system(requests_mock):
 
     # Mock the debug docker network endpoint
     requests_mock.get(
-        f"http+docker://localhost/v1.46/containers/{container_name}/json",
+        f"http+docker://localhost/v1.47/containers/{container_name}/json",
         json={
             "NetworkSettings": {
                 "Networks": {"stella-app_default": {"IPAddress": container_name}}
