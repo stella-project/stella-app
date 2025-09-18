8ff859a3ab9fb85e53912856295bee3c65566793
jueri <jueri.keller@th-koeln.de>
2025-09-18 16:21:19 +0200

    fix cache to add results based on old key

--- web/app/services/proxy_service.py
+++ web/app/services/proxy_service.py
@@ -167,6 +167,8 @@ async def make_results(
     if cached_result:
         current_app.logger.debug("Cache hit")
         return cached_result
+    else:
+        current_app.logger.debug("Cache misssssssssssssssssssss")
 
     if current_app.config["INTERLEAVE"]:
         # get the base system
@@ -221,7 +223,7 @@ async def make_results(
         )
         response = build_response(ranking, container_name, result=result)
 
-    if response.get("stella-hits"):
+    if response.get("hits"):
         await current_app.cache.set(
             cache_key, response, ttl=current_app.config["SESSION_EXPIRATION"]
         )
