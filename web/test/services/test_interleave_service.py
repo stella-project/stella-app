8e8c62c6ca6ae78a954afc5aec6916dfb0ecc23e
Jüri Keller <jueri.keller@th-koeln.de>
2026-04-27 08:21:39 +0200

    format

--- web/test/services/test_interleave_service.py
+++ web/test/services/test_interleave_service.py
@@ -1,6 +1,5 @@
-from copy import deepcopy
-
 from app.services.interleave_service import interleave_rankings, team_draft_interleave
+
 from ..create_test_data import create_results
 
 possible_results = [
@@ -66,7 +65,11 @@ def test_tdi():
         2: "doc12",
         3: "doc13",
     }
-    interleaved_results = team_draft_interleave([v for v in item_dict_base.values()], [v for v in item_dict_exp.values()], rpp=len(item_dict_base))
+    interleaved_results = team_draft_interleave(
+        [v for v in item_dict_base.values()],
+        [v for v in item_dict_exp.values()],
+        rpp=len(item_dict_base),
+    )
 
     # since the interleaving is random if both systems were utelized the same, there are multiple possible results
     assert interleaved_results in possible_results
@@ -89,7 +92,9 @@ def test_interleave_rankings(sessions):
         3: {"docid": "doc13", "type": "EXP"},
     }
 
-    interleaved_results = interleave_rankings(results_exp, results_base, 'ranking', rpp=len(results_base.items))
+    interleaved_results = interleave_rankings(
+        results_exp, results_base, "ranking", rpp=len(results_base.items)
+    )
     # The interleaved dict is added to the Result object which is a JSON type. Therefore the keys are konverted from int to str. Here we revert this conversion.
     interleaved_results = {int(k): v for k, v in interleaved_results.items.items()}
     assert interleaved_results in possible_results
@@ -108,10 +113,11 @@ def test_interleave_rankings_exp_fail(sessions):
     results_exp = result["ranker"]
     results_exp.items = {}
 
-    interleaved_results = interleave_rankings(results_exp, results_base, 'ranking', rpp=len(results_base.items))
+    interleaved_results = interleave_rankings(
+        results_exp, results_base, "ranking", rpp=len(results_base.items)
+    )
 
     assert interleaved_results.items == results_base.items
-
 
 
 def test_interleave_rankings_less_results_than_expected(sessions):
@@ -131,11 +137,12 @@ def test_interleave_rankings_less_results_than_expected(sessions):
         2: {"docid": "doc12", "type": "EXP"},
     }
 
-    interleaved_results = interleave_rankings(results_exp, results_base, 'ranking', rpp=6)
+    interleaved_results = interleave_rankings(
+        results_exp, results_base, "ranking", rpp=6
+    )
     interleaved_results = {int(k): v for k, v in interleaved_results.items.items()}
 
     assert interleaved_results in possible_results
-
 
 
 def test_interleave_rankings_unbalanced(sessions):
@@ -154,7 +161,10 @@ def test_interleave_rankings_unbalanced(sessions):
         1: {"docid": "doc11", "type": "EXP"},
     }
 
-    interleaved_results = interleave_rankings(results_exp, results_base, 'ranking', rpp=4)
+    interleaved_results = interleave_rankings(
+        results_exp, results_base, "ranking", rpp=4
+    )
     interleaved_results = {int(k): v for k, v in interleaved_results.items.items()}
 
+    assert interleaved_results in possible_results
     assert interleaved_results in possible_results
