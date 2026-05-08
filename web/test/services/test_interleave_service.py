a3dafe096b7ee8d190ef6a800cb77674b47a7cac
Jüri Keller <jueri.keller@th-koeln.de>
2026-05-08 18:05:46 +0200

    add test to check interleaved ranking len when more results are provided then rpp and remove nonsensical assert in number_suffles test

--- web/test/services/test_interleave_service.py
+++ web/test/services/test_interleave_service.py
@@ -1,4 +1,3 @@
-import random
 from unittest.mock import patch
 
 from app.services.interleave_service import interleave_rankings, team_draft_interleave
@@ -112,11 +111,10 @@ def test_interleave_rankings(sessions):
         3: {"docid": "doc13", "type": "EXP"},
     }
 
-
     interleaved_results = interleave_rankings(
         results_exp, results_base, "ranking", rpp=len(results_base.items)
     )
-    # The interleaved dict is added to the Result object which is a JSON type. Therefore the keys are konverted from int to str. Here we revert this conversion.
+    # The interleaved dict is added to the Result object which is a JSON type. Therefore the keys are converted from int to str. Here we revert this conversion.
     interleaved_results = {int(k): v for k, v in interleaved_results.items.items()}
     assert interleaved_results in possible_results
 
@@ -166,6 +164,34 @@ def test_interleave_rankings_less_results_than_expected(sessions):
     assert interleaved_results in possible_results
 
 
+def test_interleave_rankings_more_results_than_expected(sessions):
+    result = create_results(sessions)
+
+    results_base = result["ranker_base"]
+    results_base.items = {
+        1: {"docid": "doc1", "type": "BASE"},
+        2: {"docid": "doc2", "type": "BASE"},
+        3: {"docid": "doc3", "type": "BASE"},
+        4: {"docid": "doc4", "type": "BASE"},
+        5: {"docid": "doc5", "type": "BASE"},
+    }
+
+    results_exp = result["ranker"]
+    results_exp.items = {
+        1: {"docid": "doc11", "type": "EXP"},
+        2: {"docid": "doc12", "type": "EXP"},
+        3: {"docid": "doc13", "type": "EXP"},
+        4: {"docid": "doc14", "type": "EXP"},
+        5: {"docid": "doc15", "type": "EXP"},
+    }
+
+    interleaved_results = interleave_rankings(
+        results_exp, results_base, "ranking", rpp=2
+    )
+
+    assert len(interleaved_results.items) == 2
+
+
 def test_interleave_rankings_unbalanced(sessions):
     result = create_results(sessions)
 
@@ -188,7 +214,6 @@ def test_interleave_rankings_unbalanced(sessions):
     interleaved_results = {int(k): v for k, v in interleaved_results.items.items()}
 
     assert interleaved_results in possible_results
-    assert interleaved_results in possible_results
 
 
 class TestInterleaving:
@@ -199,13 +224,6 @@ class TestInterleaving:
 
         # Run the algorithm for a max of 4 items
         res = team_draft_interleave(ranking_a, ranking_b, rpp=4)
-
-        assert res == {
-            1: {"docid": "d1", "type": "BASE"},
-            2: {"docid": "d2", "type": "EXP"},
-            3: {"docid": "d3", "type": "BASE"},
-            4: {"docid": "d4", "type": "EXP"},
-        }
 
         # Both rankings are identical. Therefore, we have two ties and need to shuffle twice, first at rank 1 and second at rank 3.
         assert mock_shuffle.call_count == 2
@@ -234,7 +252,7 @@ class TestInterleaving:
 
             if pattern_key in pattern_counts:
                 pattern_counts[pattern_key] += 1
-
+        assert sum(pattern_counts.values()) == iterations
 
         # Out of 1000 runs, a perfect 25% distribution is 250.
         # We allow a safe variance window between 190 and 310.
@@ -243,11 +261,10 @@ class TestInterleaving:
 
         for pattern, count in pattern_counts.items():
             # This catches the impossible 0% cases in Balanced Interleaving
-            assert count >= lower_bound, (
-                f"Failed: {pattern} occurred {count} times (Expected ~250)."
-            )
+            assert (
+                count >= lower_bound
+            ), f"Failed: {pattern} occurred {count} times (Expected ~250)."
             # This catches the inflated 50% cases in Balanced Interleaving
-            assert count <= upper_bound, (
-                f"Failed: {pattern} occurred {count} times (Expected ~250)."
-            )
-            
+            assert (
+                count <= upper_bound
+            ), f"Failed: {pattern} occurred {count} times (Expected ~250)."
